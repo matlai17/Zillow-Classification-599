5c2e73ce0d8ed742692bb5595db4e7c712652b03
Matthew Lai <matlai17@gmail.com>
2015-05-23 04:09:16 -0700

    Copied parser to static method to allow access by other classes.

--- src/Parser/ZillowParser.java
+++ src/Parser/ZillowParser.java
@@ -35,23 +35,21 @@ public class ZillowParser {
                 String[] houseElements = line.split(csvDelimiter);
                 // Below is the order for features in ZillowDataTrain
                 // ZId,StreetAddress,City,State,ZipCode,SoldPrice,Bedroom,Bathroom,Area,Age,Zestimate,SchoolElem,SchoolMiddle,SchoolHigh
-                House house = new House();
-                HouseAddress address = new HouseAddress();
-                house.setZid(Long.parseLong(houseElements[0]));
-                address.setstreetAddress(houseElements[1]);
-                address.setCity(houseElements[2]);
-                address.setState(houseElements[3]);
-                address.setZip(houseElements[4]);
-                house.setPriceSold(Double.parseDouble(houseElements[5]));
-                house.setNoOfBedroom(Integer.parseInt(houseElements[6]));
-                house.setNoOfBathroom(Double.parseDouble(houseElements[7]));
-                house.setArea(Double.parseDouble(houseElements[8]));
-                house.setBuiltYear(Integer.parseInt(houseElements[9]));
-                house.setZestimate(Double.parseDouble(houseElements[10]));
-                house.setSchoolElem(Integer.parseInt(houseElements[11]));
-                house.setSchoolMid(Integer.parseInt(houseElements[12]));
-                house.setSchoolHigh(Integer.parseInt(houseElements[13]));
-                house.setHouseAddress(address);
+                House house = new House(
+                        Long.parseLong(houseElements[0]),           //zid
+                        houseElements[1],                           //street
+                        houseElements[2],                           //city
+                        houseElements[3],                           //state
+                        houseElements[4],                           //zip
+                        Double.parseDouble(houseElements[5]),       //price
+                        Integer.parseInt(houseElements[6]),         //bedrooms
+                        Double.parseDouble(houseElements[7]),       //bathrooms
+                        Double.parseDouble(houseElements[8]),       //area
+                        Integer.parseInt(houseElements[9]),         //age
+                        Double.parseDouble(houseElements[10]),      //zestimate
+                        Integer.parseInt(houseElements[11]),        //elementary
+                        Integer.parseInt(houseElements[12]),        //middle
+                        Integer.parseInt(houseElements[13]));       //high
                 houseList.add(house);
 
             }
