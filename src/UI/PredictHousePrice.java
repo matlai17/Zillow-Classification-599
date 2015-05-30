5cc505d0176de9678db3005c890188fe7078ee45
Matthew Lai <matlai17@gmail.com>
2015-05-30 03:17:49 -0700

    Fixed the table problem. Changed some aesthetic things and added keylistener to quit on escape (personal thing).

--- src/UI/PredictHousePrice.java
+++ src/UI/PredictHousePrice.java
@@ -13,17 +13,33 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import Controller.Controller;
+import java.awt.ScrollPane;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
 
 public class PredictHousePrice {
 
 	private JFrame frame;
 	private JTable table;
+        private JScrollPane scrollPane;
 	ArrayList<String[]> test = new ArrayList<String[]>();
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+            
+            //Change how the UI looks like. You can use different UI types if you think something else might look better.
+            try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); } 
+            catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) 
+            { Logger.getLogger(PredictHousePrice.class.getName()).log(Level.SEVERE, null, ex); }
+            
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -52,42 +68,55 @@ public class PredictHousePrice {
 		ArrayList<String[]> filteredData = new ArrayList<String[]>();
 
 		for (int i = 0; i < data.size(); i++)
-			if (data.get(i)[1].equals(city)) {
+			if (data.get(i)[1].equals(city) || city.equals("All Cities")) {
 				filteredData.add(data.get(i));
 			}
 
 		String[][] tblData = new String[filteredData.size()][filteredData
 				.get(0).length];
 		for (int i = 0; i < filteredData.size(); i++)
-			if (filteredData.get(i)[1].equals(city)) {
+			if (filteredData.get(i)[1].equals(city) || city.equals("All Cities")) {
 				for (int j = 0; j < filteredData.get(0).length; j++) {
 					tblData[i][j] = filteredData.get(i)[j];
 				}
 			}
-		table.setBounds(0, 0, 700, 700);
-		JScrollPane scrollPane = new JScrollPane(table);
-		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-		frame.setVisible(true);
+                table.setModel(new DefaultTableModel(tblData, columnNames));
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+                
 		table = new JTable();
+                table.setBounds(0, 0, 700, 700);
+                table.setEnabled(false);
+                
+                scrollPane = new JScrollPane(table);
+                
+		frame = new JFrame();
 		frame.setBounds(0, 0, 800, 800);
-		frame.getContentPane().setLayout(new BorderLayout());
+		frame.getContentPane().setLayout(new BorderLayout(0, 10));
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 		// frame.getContentPane().add(new JButton("Left"), BorderLayout.WEST);
 		// frame.getContentPane().add(new JButton("Center"),
 		// BorderLayout.CENTER);
-
+                
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                KeyListener kL = new java.awt.event.KeyAdapter() {
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+                        int key = e.getKeyCode();
+                        if(key == KeyEvent.VK_ESCAPE) frame.dispose();
+                    }
+                };
 
 		Controller.main(null);
 		test = Controller.res;
 		ArrayList<String> city = new ArrayList<String>();
 		String prev = "";
+                city.add("All Cities");
 		for (String string[] : test) {
 			if (!string[1].equals(prev)) {
 				city.add(string[1]);
@@ -106,6 +135,16 @@ public class PredictHousePrice {
 		comboBox.setBounds(104, 0, 298, 20);
 		frame.getContentPane().add(lbl, BorderLayout.BEFORE_FIRST_LINE);
 		frame.getContentPane().add(comboBox, BorderLayout.NORTH);
-
+                
+                showtable(test, comboBox.getSelectedItem().toString());
+                
+                frame.addKeyListener(kL);
+                scrollPane.addKeyListener(kL);
+                table.addKeyListener(kL);
+                comboBox.addKeyListener(kL);
+                
+                table.getColumnModel().getColumn(0).setPreferredWidth(200);
+                
+		frame.setVisible(true);
 	}
 }
