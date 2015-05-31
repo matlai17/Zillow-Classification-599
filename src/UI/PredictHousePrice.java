a5164a20d8dbf9dbc29642bd1db1c9de829aa970
Arun <arunsinghit@gmail.com>
2015-05-30 19:35:09 -0700

    Added features to JTable

--- src/UI/PredictHousePrice.java
+++ src/UI/PredictHousePrice.java
@@ -4,42 +4,45 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
-import Controller.Controller;
-import java.awt.ScrollPane;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
+
+import Controller.Controller;
 
 public class PredictHousePrice {
 
 	private JFrame frame;
 	private JTable table;
-        private JScrollPane scrollPane;
+	private JScrollPane scrollPane;
 	ArrayList<String[]> test = new ArrayList<String[]>();
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-            
-            //Change how the UI looks like. You can use different UI types if you think something else might look better.
-            try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); } 
-            catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) 
-            { Logger.getLogger(PredictHousePrice.class.getName()).log(Level.SEVERE, null, ex); }
-            
+
+		// Change how the UI looks like. You can use different UI types if you
+		// think something else might look better.
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException ex) {
+			Logger.getLogger(PredictHousePrice.class.getName()).log(
+					Level.SEVERE, null, ex);
+		}
+
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -62,7 +65,9 @@ public class PredictHousePrice {
 	}
 
 	public void showtable(ArrayList<String[]> data, String city) {
-		String[] columnNames = { "Address", "City", "Price sold", "Zestimate",
+		String[] columnNames = { "Address", "City", "Bedroom", "Bathroom",
+				"Price/Sqft", "Elementary", "Middle", "High School",
+				"Year Built", "Area", "Price sold", "Zestimate",
 				"ANN Prediction", "NB Prediction" };
 
 		ArrayList<String[]> filteredData = new ArrayList<String[]>();
@@ -75,49 +80,50 @@ public class PredictHousePrice {
 		String[][] tblData = new String[filteredData.size()][filteredData
 				.get(0).length];
 		for (int i = 0; i < filteredData.size(); i++)
-			if (filteredData.get(i)[1].equals(city) || city.equals("All Cities")) {
+			if (filteredData.get(i)[1].equals(city)
+					|| city.equals("All Cities")) {
 				for (int j = 0; j < filteredData.get(0).length; j++) {
 					tblData[i][j] = filteredData.get(i)[j];
 				}
 			}
-                table.setModel(new DefaultTableModel(tblData, columnNames));
-                table.getColumnModel().getColumn(0).setPreferredWidth(200);
+		table.setModel(new DefaultTableModel(tblData, columnNames));
+		table.getColumnModel().getColumn(0).setPreferredWidth(200);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-                
+
 		table = new JTable();
-                table.setBounds(0, 0, 700, 700);
-                table.setEnabled(false);
-                
-                scrollPane = new JScrollPane(table);
-                
+		table.setBounds(0, 0, 700, 700);
+		table.setEnabled(false);
+		scrollPane = new JScrollPane(table);
+
 		frame = new JFrame();
-		frame.setBounds(0, 0, 800, 800);
+		frame.setBounds(0, 0, 1200, 700);
 		frame.getContentPane().setLayout(new BorderLayout(0, 10));
 		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 		// frame.getContentPane().add(new JButton("Left"), BorderLayout.WEST);
 		// frame.getContentPane().add(new JButton("Center"),
 		// BorderLayout.CENTER);
-                
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
-                KeyListener kL = new java.awt.event.KeyAdapter() {
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-                        int key = e.getKeyCode();
-                        if(key == KeyEvent.VK_ESCAPE) frame.dispose();
-                    }
-                };
+
+		KeyListener kL = new java.awt.event.KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				int key = e.getKeyCode();
+				if (key == KeyEvent.VK_ESCAPE)
+					frame.dispose();
+			}
+		};
 
 		Controller.main(null);
 		test = Controller.res;
 		ArrayList<String> city = new ArrayList<String>();
 		String prev = "";
-                city.add("All Cities");
+		city.add("All Cities");
 		for (String string[] : test) {
 			if (!string[1].equals(prev)) {
 				city.add(string[1]);
@@ -125,6 +131,7 @@ public class PredictHousePrice {
 			}
 		}
 		JComboBox comboBox = new JComboBox(city.toArray());
+		// JComboBox comboBox1 = new JComboBox(city.toArray());
 		JLabel lbl = new JLabel("Select City");
 		comboBox.addActionListener(new ActionListener() {
 			@Override
@@ -134,16 +141,18 @@ public class PredictHousePrice {
 		});
 		lbl.setBounds(5, 0, 98, 20);
 		comboBox.setBounds(104, 0, 298, 20);
+		// comboBox.setBounds(208, 0, 298, 20);
 		frame.getContentPane().add(lbl, BorderLayout.BEFORE_FIRST_LINE);
 		frame.getContentPane().add(comboBox, BorderLayout.NORTH);
-                
-                showtable(test, comboBox.getSelectedItem().toString());
-                
-                frame.addKeyListener(kL);
-                scrollPane.addKeyListener(kL);
-                table.addKeyListener(kL);
-                comboBox.addKeyListener(kL);
-                
+		// frame.getContentPane().add(comboBox1, BorderLayout.AFTER_LAST_LINE);
+
+		showtable(test, comboBox.getSelectedItem().toString());
+
+		frame.addKeyListener(kL);
+		scrollPane.addKeyListener(kL);
+		table.addKeyListener(kL);
+		comboBox.addKeyListener(kL);
+
 		frame.setVisible(true);
 	}
 }
